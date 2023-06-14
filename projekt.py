924a048ddce4ddd4474f0476a6e7efccb126b89a
Jakub <cubus331122@gmail.com>
2023-06-14 16:40:18 +0200

    Parsowanie

---
+++ projekt.py
@@ -0,0 +1,15 @@
+import argparse
+import json
+import yaml
+import xml.etree.ElementTree as ET
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file", help="Input file path")
+parser.add_argument("output_file", help="Output file path")
+args = parser.parse_args()
+
+input_file = args.input_file
+output_file = args.output_file
+
+input_extension = os.path.splitext(input_file)[1].lower()
+output_extension = os.path.splitext(output_file)[1].lower()
